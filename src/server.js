9302aca4168d12a27157ce38155ee61903262a16
Moong <343363623@qq.com>
2023-03-22 14:09:45 +0800

    代理服务2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import axios from 'axios';
+
 const app = new Hono();
 
 app.get("/", (ctx) => ctx.text("Hello world, this is Hono!!"));
@@ -11,6 +13,27 @@ app.get('/user', (c) => {
   // const name = c.req.param('name')
   // console.log('name :>> ', name);
   return c.text(JSON.stringify(query, null, '  '));
+})
+
+app.get('/proxy', async (c) => {
+  const userAgent = c.req.header('User-Agent');
+  const query = c.req.query();
+  // console.log('query :>> ', query);
+  // console.log('userAgent :>> ', userAgent);
+  const { url = undefined } = query;
+  console.log('url :>> ', url);
+
+
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'User-Agent': " Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0" },
+    redirect: 'follow',
+    url
+    // referrerPolicy: 'no-referrer'
+  };
+  const response = await axios(requestOptions);
+  return response;
+  // return c.text(JSON.stringify(query, null, '  '));
 })
 
 
